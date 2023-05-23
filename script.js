4993615d47c3d8f879699987cddc5498fbca83b1
pdhoangvu <pdhoangvu@gmail.com>
2023-05-23 14:05:04 +0700

    Create an html boilerplate and link it with the js file

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const add = function(a, b) {
 const subtract = function(a, b) {
 	return a - b;
 };
+//Create a mulitply function 
+const mulitply = function(a, b) {
+	return a * b;
+};
+//Create a divide function 
+const divide = function(a, b) {
+	return b===0 ? "Can't divide with zero" : a / b;
+};
